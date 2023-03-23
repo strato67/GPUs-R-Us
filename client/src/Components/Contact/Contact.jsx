a588065eb07ea3391bfe51f80c3c58fa5dd5513b
strato67 <saaranshsharma28@gmail.com>
2023-03-22 21:32:07 -0400

    update contact page

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -2,16 +2,18 @@ export default function Contact() {
   return (
     <>
       <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+        <div className="hero-content flex-col lg:flex-row">
+          <div className="text-center lg:text-left md:w-2/3 lg:w-1/2">
+            <h1 className="text-5xl font-bold">Contact Us</h1>
+            <p className="py-6 text-lg">
+              Let's connect! Fill out the form with your name, email
+              address, and message, and we'll get back to you as soon as
+              possible. Whether you have a question, concern, or just want to
+              say hello, we're always happy to hear from our customers. So don't
+              hesitate, fill out the form and let us know how we can assist you!
             </p>
           </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
             <div className="card-body">
               <div className="form-control">
                 <label className="label">
@@ -21,25 +23,20 @@ export default function Contact() {
                   type="text"
                   placeholder="email"
                   className="input input-bordered"
+                  required={true}
                 />
               </div>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Password</span>
+                  <span className="label-text">Message</span>
                 </label>
-                <input
-                  type="text"
-                  placeholder="password"
-                  className="input input-bordered"
-                />
+                <textarea className="textarea textarea-primary" placeholder="enter your message here" required={true}></textarea>
                 <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
+
                 </label>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
+                <button className="btn btn-primary" >Submit</button>
               </div>
             </div>
           </div>
