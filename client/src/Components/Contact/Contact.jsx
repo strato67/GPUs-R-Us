08717d9189bf6c2d6e007af6ffce83a59943a77b
strato67 <saaranshsharma28@gmail.com>
2023-03-25 19:36:43 -0400

    Contact page form

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -1,43 +1,86 @@
+import { useState } from "react";
+
 export default function Contact() {
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormSubmitted(true);
+    console.log(e.target);
+  };
+
   return (
     <>
+      <div
+        className={`alert alert-success shadow-lg md:pd-5 ${
+          formSubmitted
+            ? `block transition delay-150 duration-300 ease-in-out`
+            : `hidden`
+        }`}
+      >
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current flex-shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>Your message has been submitted!</span>
+        </div>
+      </div>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row">
           <div className="text-center lg:text-left md:w-2/3 lg:w-1/2">
             <h1 className="text-5xl font-bold">Contact Us</h1>
             <p className="py-6 text-lg">
-              Let's connect! Fill out the form with your name, email
-              address, and message, and we'll get back to you as soon as
-              possible. Whether you have a question, concern, or just want to
-              say hello, we're always happy to hear from our customers. So don't
-              hesitate, fill out the form and let us know how we can assist you!
+              Let's connect! Fill out the form with your email address and
+              message, and we'll get back to you as soon as possible. Whether
+              you have a question, concern, or just want to say hello, we're
+              always happy to hear from our customers. So don't hesitate, fill
+              out the form and let us know how we can assist you!
             </p>
           </div>
           <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
             <div className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="email"
-                  className="input input-bordered"
-                  required={true}
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Message</span>
-                </label>
-                <textarea className="textarea textarea-primary" placeholder="enter your message here" required={true}></textarea>
-                <label className="label">
-
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary" >Submit</button>
-              </div>
+              <form onSubmit={handleSubmit} method="post">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="email"
+                    className="input input-bordered"
+                    required={true}
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Message</span>
+                  </label>
+                  <textarea
+                    className="textarea textarea-primary"
+                    placeholder="enter your message here"
+                    required={true}
+                  ></textarea>
+                  <label className="label"></label>
+                </div>
+                <div className="form-control mt-6">
+                  <input
+                    type="submit"
+                    className="btn btn-primary"
+                    value="Submit"
+                    disabled={formSubmitted}
+                  />
+                </div>
+              </form>
             </div>
           </div>
         </div>
