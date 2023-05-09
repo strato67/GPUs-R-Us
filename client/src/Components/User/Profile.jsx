bf214161892ed8331a9dd658d76cb88fe80cd5f1
strato67 <saaranshsharma28@gmail.com>
2023-05-09 19:40:13 -0400

    profile changes

--- client/src/Components/User/Profile.jsx
+++ client/src/Components/User/Profile.jsx
@@ -21,5 +21,23 @@ export default function Profile() {
     getuserInfo();
   }, []);
 
-  return <>{userInfo && <ProfileCard userInfo={userInfo} />}</>;
+  return (
+    <>
+      <div className=" min-h-screen bg-base-200">
+        <div className="flex justify-center py-5">
+          {userInfo && <ProfileCard userInfo={userInfo} />}
+        </div>
+
+        <div className="flex justify-center">
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title">Wishlist</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- client/src/Components/User/ProfileCard.jsx
+++ client/src/Components/User/ProfileCard.jsx
@@ -4,7 +4,7 @@ export default function ProfileCard({ userInfo }) {
       <div className="card lg:card-side bg-base-100 shadow-xl">
         <figure>
           <div className="avatar placeholder">
-            <div className="bg-neutral-focus text-neutral-content rounded-full w-24">
+            <div className="bg-neutral-focus text-neutral-content rounded-full w-24 h-24">
               <span className="text-3xl">{userInfo.user.username}</span>
             </div>
           </div>
@@ -12,9 +12,7 @@ export default function ProfileCard({ userInfo }) {
         <div className="card-body">
           <h2 className="card-title">{userInfo.user.username}</h2>
           <p>{`Joined on: ${userInfo.user.joined}`}</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Listen</button>
-          </div>
+
         </div>
       </div>
     </>
