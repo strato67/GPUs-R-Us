67114c5a1e0159fa4a07dcbedbfd35ab96b01ba4
strato67 <saaranshsharma28@gmail.com>
2023-06-05 22:02:08 -0400

    item collapse

--- client/src/Components/Shop/ItemPage.jsx
+++ client/src/Components/Shop/ItemPage.jsx
@@ -67,35 +67,25 @@ export default function ItemPage() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col md:flex-row pb-4 px-4 justify-center ">
-          <div className="join join-vertical w-full">
-            <div className="collapse collapse-arrow join-item border border-base-300">
-              <input type="radio" name="my-accordion-4" checked="checked" />
-              <div className="collapse-title text-xl font-medium">
-                Click to open this one and close others
-              </div>
+
+        <div className="flex flex-col pb-4 px-4 justify-center ">
+          <div className="join join-vertical  ">
+            <details className="collapse bg-base-200 collapse-plus">
+              <summary className="collapse-title text-xl font-medium">
+                Specifications
+              </summary>
               <div className="collapse-content">
-                <p>hello</p>
+                <p>content</p>
               </div>
-            </div>
-            <div className="collapse collapse-arrow join-item border border-base-300">
-              <input type="radio" name="my-accordion-4" />
-              <div className="collapse-title text-xl font-medium">
-                Click to open this one and close others
-              </div>
+            </details>
+            <details className="collapse bg-base-200 collapse-plus">
+              <summary className="collapse-title text-xl font-medium">
+                Reviews
+              </summary>
               <div className="collapse-content">
-                <p>hello</p>
+                <p>content</p>
               </div>
-            </div>
-            <div className="collapse collapse-arrow join-item border border-base-300">
-              <input type="radio" name="my-accordion-4" />
-              <div className="collapse-title text-xl font-medium">
-                Click to open this one and close others
-              </div>
-              <div className="collapse-content">
-                <p>hello</p>
-              </div>
-            </div>
+            </details>
           </div>
         </div>
       </div>
