0cdf6ae95cfc98c73a70615fc45bb8c5e211b461
strato67 <saaranshsharma28@gmail.com>
2023-06-07 22:14:40 -0400

    mapping specs

--- client/src/Components/Shop/ItemPage.jsx
+++ client/src/Components/Shop/ItemPage.jsx
@@ -25,7 +25,7 @@ export default function ItemPage() {
     <>
       {itemInfo && (
         <div className=" bg-base-200">
-          <div className="flex flex-col md:flex-row pb-4 px-4 lg:space-x-6 justify-center md:py-12 ">
+          <div className="flex flex-col md:flex-row pb-4 px-4 lg:space-x-6 justify-center md:py-12  ">
             <div className="py-8 md:py-12 h-fit md:hidden">
               <h1 className="text-3xl ">{itemInfo.product.name}</h1>
             </div>
@@ -33,14 +33,14 @@ export default function ItemPage() {
             <ItemCarousel />
 
             <div className="flex flex-col justify-center w-fit md:px-4 lg:w-96 md:py-8">
-              <h1 className="text-3xl hidden md:inline">{itemInfo.product.name}</h1>
+              <h1 className="text-3xl hidden md:inline">
+                {itemInfo.product.name}
+              </h1>
               <div className="badge badge-secondary py-6 px-4 my-4 text-3xl font-semibold mb-4">
-              ${(Math.round(itemInfo.product.price * 100) / 100).toFixed(2)}
+                ${(Math.round(itemInfo.product.price * 100) / 100).toFixed(2)}
               </div>
               <h2 className="text-xl font-bold">Overview</h2>
-              <p className="py-4">
-                {itemInfo.product.description}
-              </p>
+              <p className="py-4">{itemInfo.product.description}</p>
               <div className="flex justify-center w-full ">
                 <button className="btn btn-primary w-1/2 mx-1 ">
                   {" "}
@@ -67,18 +67,31 @@ export default function ItemPage() {
             </div>
           </div>
 
-          <div className="flex flex-col pb-4 px-4 justify-center ">
-            <div className="join join-vertical  ">
-              <details className="collapse bg-base-200 collapse-plus">
-                <summary className="collapse-title text-xl font-medium">
+          <div className="flex flex-col pb-4 px-4 items-center w-full">
+            <div className="join join-vertical w-full lg:w-3/5">
+              <details className="collapse bg-base-200 collapse-plus shadow-xl">
+                <summary className="collapse-title text-xl font-bold bg-primary text-primary-content">
                   Specifications
                 </summary>
                 <div className="collapse-content">
-                  <p>content</p>
+                  <div className="card w-full bg-base-200 ">
+                    <div className="card-body">
+                      {itemInfo.product.specs.map((spec, index) => {
+                        return (
+                          <div key={index}>
+                            <h2 className="card-title">{spec.name}</h2>
+                            <ul className="list-disc">
+                              <li>{spec.spec}</li>
+                            </ul>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
                 </div>
               </details>
-              <details className="collapse bg-base-200 collapse-plus">
-                <summary className="collapse-title text-xl font-medium">
+              <details className="collapse bg-base-200 collapse-plus shadow-xl">
+                <summary className="collapse-title text-xl font-bold bg-primary text-primary-content">
                   Reviews
                 </summary>
                 <div className="collapse-content">
