8d8761a266d8b3a82368cd646016b19028a5de21
strato67 <saaranshsharma28@gmail.com>
2023-07-24 21:48:33 -0400

    new hooks

---
+++ client/src/Hooks/useCart.jsx
@@ -0,0 +1,13 @@
+export default function useCart() {
+  const getCart = async () => {};
+
+  const addToCart = async () => {};
+
+  const removeFromCart = async () => {};
+
+  const updateCart = async () => {};
+
+  const emptyCart = async () => {};
+
+  return { getCart, addToCart, removeFromCart, updateCart, emptyCart };
+}
