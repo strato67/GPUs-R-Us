ab0a1aeea426c0070cffce846571672ec5a97f72
strato67 <saaranshsharma28@gmail.com>
2023-07-24 21:47:26 -0400

    frontend cart starts :)

--- client/src/Components/Cart/Cart.jsx
+++ client/src/Components/Cart/Cart.jsx
@@ -1,15 +1,21 @@
+import useAuthContext from "../../Hooks/useAuthContext";
 import Empty from "./Empty";
 import CartCard from "./CartCard";
 
 export default function Cart() {
+  const { user } = useAuthContext();
+
   return (
     <>
-      {false && <Empty />}
-      <div className="flex flex-col bg-base-200 py-8 justify-center gap-6 px-8">
-        <CartCard />
-        <CartCard />
-        <CartCard />
-      </div>
+      {!user && <Empty />}
+
+      {user && (
+        <div className="flex flex-col bg-base-200 py-8 justify-center gap-6 px-8">
+          <CartCard />
+          <CartCard />
+          <CartCard />
+        </div>
+      )}
     </>
   );
 }
